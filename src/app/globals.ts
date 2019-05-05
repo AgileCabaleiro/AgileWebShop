bc0f3d8198ce46d2a1441c98af54437ce178af2d
ICabaleiro <cabaleiro8@gmail.com>
2019-05-05 21:51:33 +0100

    Auto-generated commit

--- src/app/globals.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class Globals {
-    public selectedStore = 'España'; //Default España - English (this need to be changed to domain selection)
-    public selectedStoreId : number = 2; // Default España - English (this need to be changed to domain selection)
-    public homeCategories = [];// Used in navbar & several components
-    public componentSelector = 'home'; // Used to know current component loaded in router-outlet (home as default)
-}
