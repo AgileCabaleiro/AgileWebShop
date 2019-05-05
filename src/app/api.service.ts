6a7c052bc1ce0c78289d194c32a1e93a40e7b28c
ICabaleiro <cabaleiro8@gmail.com>
2019-05-05 21:46:17 +0100

    Fixing cross-domain issues

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -11,7 +11,7 @@ export class ApiService{
 
 	/* Methods */
 	requestAllStores(){
-		return this.http.get('http://private-anon-d77ff3624d-gocco.apiary-mock.com/stores');
+		return this.http.get('https://private-anon-d77ff3624d-gocco.apiary-mock.com/stores');
 	}
 	requestAllCountries(storeId:number){
 		return this.http.get('https://private-anon-d77ff3624d-gocco.apiary-mock.com/stores/' + storeId + '/shops/countries');
@@ -25,7 +25,7 @@ export class ApiService{
 	}
 	/* Methods - Products */
 	requestProducts(storeId:number, filters){
-		const url = 'http://private-anon-d77ff3624d-gocco.apiary-mock.com/stores/' + storeId + '/products/search?filters=&with_text=&category_id=' + filters.categoryId + '&order=' + filters.order + '&dir=' + filters.dir + '&page=' + filters.page + '&limit=' + filters.limit;
+		const url = 'https://private-anon-d77ff3624d-gocco.apiary-mock.com/stores/' + storeId + '/products/search?filters=&with_text=&category_id=' + filters.categoryId + '&order=' + filters.order + '&dir=' + filters.dir + '&page=' + filters.page + '&limit=' + filters.limit;
 		console.log(url);
 		return this.http.get(url);
 	}
